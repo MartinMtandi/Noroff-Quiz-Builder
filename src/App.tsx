931802c7420aad5add6ede1fa024df931887f9ac
MartinMtandi <mtandimartin@gmail.com>
2025-11-05 11:53:16 +0200

    feat: add quiz preview route and component
    - Added new QuizPreview component route at /quiz-preview path
    - Imported QuizPreview component into main App router
    - Integrated QuizPreview within existing Layout wrapper component

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Index from '@/pages/Index'
 import Layout from '@/components/layout/Index'
+import QuizPreview from './components/QuizPreview'
 export default function App() {
   return (
     <Router>
       <Layout>
         <Routes>
           <Route path="/" element={<Index />} />
+          <Route path="/quiz-preview" element={<QuizPreview />} />
           {/* additional routes go here */}
         </Routes>
       </Layout>
